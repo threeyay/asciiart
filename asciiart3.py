37a4e7b89b5341cd24d725d06366dcffd9ba9b50
abhimanyu <abhimanyu@3a.(none)>
2013-08-23 10:28:54 +0530

    First Commit

---
+++ asciiart3.py
@@ -0,0 +1,46 @@
+from PIL import Image
+import random
+from bisect import bisect
+import os
+
+greyscale = [
+		" ",
+		" ",
+		".,-",
+		"_ivc=!/|\\~",
+		"gjez2]/(YL)t[+T7Vf",
+		"mdK4ZGbNDXY5P*Q",
+		"W8KMA",
+		"#%$"
+		]
+colourdepth=[36,72,108,144,180,216,252]
+def make_thumbnail(image):
+	im=Image.open(image)
+	xvalue=im.size[0]
+	yvalue=im.size[1]
+	if xvalue>160:
+		xnewvalue=160
+		ynewvalue=yvalue/(xvalue/xnewvalue)
+		im=im.resize((xnewvalue,ynewvalue),Image.BILINEAR)
+	im=im.convert("L")
+	return im
+def make_ascii(image):
+	str=''
+	im=make_thumbnail(image)
+	for y in range(0,im.size[1]):
+		for x in range(0,im.size[0]):
+			lum=255-im.getpixel((x,y))
+			row=bisect(colourdepth,lum)
+			possibles=greyscale[row]
+			str=str+possibles[random.randint(0,len(possibles)-1)]+' '
+		str=str+"\n"
+	return str
+path='/home/abhimanyu/Desktop/3a_data/Pictures/people/'
+for image in os.listdir(path):
+	print image
+	if image.endswith('.jpg'):
+		f=open('/home/abhimanyu/asciiart3/'+image.replace('.jpg',''),'w')
+	if image.endswith('.JPG'):
+		f=open('/home/abhimanyu/asciiart3/'+image.replace('.JPG',''),'w')
+	asciiart=make_ascii('/home/abhimanyu/Desktop/3a_data/Pictures/people/'+image)
+	f.write(asciiart)
